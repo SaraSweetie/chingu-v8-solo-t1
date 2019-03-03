3a4efc616581882924ad363eea669c101c5aa196
Sara Krum <sarasmith887@gmail.com>
2019-03-02 21:57:08 -0500

    animate in first 5 elements, add delay - started

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-window.onscroll = function() {animateIn()};
+let firstFive = [];
 
-function animateIn() {
+window.onload = function() {fadeEachIn(firstFive)};
+
+function fadeEachIn(firstFive) {
+  let elements = document.getElementsByClassName('element');
+
+  firstFive.push(elements);
+
+  console.log(elements);
+
+  firstFive.forEach(function(element, i) {
+	console.log(firstFive);
+	// use setTimeout to delay fadeIn of first 5 elements onload
+	setTimeout(function(){
+		//element.classList.remove("element");
+		element.classList.add("slideIn");
+	}, 200 * (i + 1));
+
+	});
+}
+
+/*window.onload = function() {fadeEachIn()};
+
+function fadeEachIn() {
+  let elements = document.getElementsByClassName('element');
+  
+  console.log(elements);
+
+  elements.forEach(function(element, i) {
+	console.log(element);
+	// use setTimeout to delay fadeIn of first 5 elements onload
+	setTimeout(function(){
+		//element.classList.remove("element");
+		element.classList.add("slideIn");
+	}, 200 * (i + 1));
+
+	});
+}*/
+
+window.onscroll = function() {animateInScroll()};
+
+function animateInScroll() {
 	// add class .slideIn to figures when scroll height is 550px
 	if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550){
 
