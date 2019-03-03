f2b6f9115cf287d0bfcc68779d6ffc270f961cab
Sara Krum <sarasmith887@gmail.com>
2019-03-02 19:58:23 -0500

    fade in 3 boxes working

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 window.onscroll = function() {animateIn()};
 
 function animateIn() {
-	// add class .slideIn to figures when scroll height is 600px
+	// add class .slideIn to figures when scroll height is 550px
 	console.log('function triggered');
-	if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600){
-		var fadeMeIn = document.getElementsByTagName('figure');
+	if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550){
 
-		console.log(fadeMeIn);
-				
-		fadeMeIn.classList.remove("hidden");
-		//fadeMeIn.classList.add("slideIn");
-		fadeMeIn.className = 'slideIn';
-		
-		console.log('>600');
+		let fadeIn = document.querySelectorAll('figure');
+
+		fadeIn.forEach(function(element, i) {
+			// use setTimeout to delay fadeIn forEach 0.4s
+			setTimeout(function(){
+				element.classList.remove("hidden");
+				element.classList.add("slideIn");
+			}, 400 * (i + 1));
+		});
 	} 
 }
